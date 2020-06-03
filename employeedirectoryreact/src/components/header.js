3fcc849ed481de65bedc6aca3e890fda13cb560e
luke williams <jluke94williams@gmail.com>
2020-06-03 19:31:36 -0400

    Added a HEader/Navbar

---
+++ employeedirectoryreact/src/components/header.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Header() {
+    return (
+        <nav>
+            <div className="nav-wrapper">
+                <a className="brand-logo center">Employee Directory</a>
+                <ul id="nav-mobile" className="left hide-on-med-and-down">
+                </ul>
+            </div>
+        </nav>
+      );
+
+}
+
+export default Header;
