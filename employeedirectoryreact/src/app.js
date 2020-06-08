f35b77a367d3fa0ba419f7e156451e50caec0d92
luke williams <jluke94williams@gmail.com>
2020-06-08 18:46:39 -0400

    Added additional info for search

--- employeedirectoryreact/src/app.js
+++ employeedirectoryreact/src/app.js
@@ -18,11 +18,38 @@ class App extends Component {
           .catch(err => console.log(err));
     };
 
+    handleInputChange = event => {
+        const value = event.target.value;
+        const name = event.target.name;
+        this.setState({
+          [name]: value
+        });
+    
+        //creating a new array by taking in new search value by using filter
+        let newResult = this.state.result.filter(employee =>{
+          // console.log(employee.name.first.toLowerCase().indexOf(this.state.search), employee.name,employee.name.last.toLowerCase().indexOf(this.state.search))
+          return employee.name.first.toLowerCase().indexOf(value)>-1 || employee.name.last.toLowerCase().indexOf(value)>-1
+        })
+        //this updates the existing result state
+        this.setState({
+          result: newResult
+        })
+        //conditional statement to load the original results once search length is at 0
+        if (value.length===0){
+          API.users()
+          .then(res => this.setState({ result: res.data.results }))
+          .catch(err => console.log(err));
+        }
+    
+    };
+
     render () {
         return (
             <div className="container">
                 <Header />
-                <Search />
+                <Search value={this.state.search}
+                handleInputChange={this.handleInputChange}
+                />
                 <table className= "striped bordered">
                     <thead>
                         <tr>
